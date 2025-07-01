a476d8db0ddff30c0c93919048ad0d03b1c8d377
rifqi856 <rifqifahmi680@gmail.com>
2025-07-01 09:09:32 +0700

    upgrade 11

---
+++ oop.js
@@ -0,0 +1,19 @@
+// !Introduction
+
+// ^ Secara umum tipe data terbagi menjadi 2, yaitu tipe data primitive dan non primitive.
+// ? Tipe data primitive adalah tipe data yang hanya dipakai untuk menyimpan data seperti: byte, short, int, long, float, double, boolean dan char.
+// ? Tipe data non primitive adalah tipe data yang selain dipakai untuk menyimpan data, tetapi juga mempunyai metode bawaan (built-in method) untuk dilakukan modifikasi dan operasi oleh programmer, seperti: String, Array, Class dan Object.
+
+//! Apa itu OOP?
+// ? OOP adalah suatu paradigma atau konsep di dunia pemrograman yang berorientasikan pada objek. Biasanya objek memiliki 2 komponen, yaitu ciri-ciri dari objek tersebut (properti) dan kemampuan yang dapat dilakukan oleh objek tersebut (method).
+
+// ! OOP memiliki 4 pilar utama yaitu:
+
+// ^ Inheritance
+// & Polymorphism
+// ~ Encapsulation
+// * Abstraction
+
+// ! 📝  Catatan:
+
+// ! JavaScript tidak sepenuhnya mendukung konsep OOP, tapi kamu akan mempelajari pemahaman dasar dari paradigma OOP.
