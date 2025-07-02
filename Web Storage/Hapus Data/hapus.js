b946702b7b17e249c0ddf45a0f4958918c497e44
rifqi856 <rifqifahmi680@gmail.com>
2025-07-03 05:22:50 +0700

    upgrade27

---
+++ Web Storage/Hapus Data/hapus.js
@@ -0,0 +1,16 @@
+// ! Local Storage - Menghapus Data
+// ? Untuk menghapus data yang telah tersimpan pada local storage, kita dapat menggunakan method removeItem() yang membutuhkan 1 parameter. Parameter tersebut adalah key dari data yang ingin kita hapus.
+
+// menghapus key tertentu
+localStorage.removeItem("key");
+
+// menghapus semua key
+localStorage.clear();
+
+// cth
+
+// Menghapus data dengan key "search"
+localStorage.removeItem("search");
+
+// Menghapus data dengan key "hewan"
+localStorage.removeItem("hewan");
--- Web Storage/mengambil Data/storage.js
+++ Web Storage/mengambil Data/storage.js
@@ -9,3 +9,15 @@ localStorage.getItem('key');
  var searchList = JSON.parse(localStorage.getItem("searchKey")) || [];
 
 //  * Maksud dari kode ini adalah variabel searchList nilai default-nya adalah data dari local storage dengan key searchKey. Namun jika data pada local storage dengan key searchKey tidak ada, maka isi variabel searchList dengan array kosong []. Lalu disini kita menggunakan JSON.parse() yang merupakan kebalikan dari JSON.stringify(), yaitu untuk mengubah string menjadi bentuk array kembali.
+
+
+//  cth
+
+// Langkah 1: Ambil data dari localStorage (masih berupa string)
+let getHewanKakiEmpat = localStorage.getItem("hewan");
+
+// Langkah 2: Ubah data dari string JSON ke array
+let hewanKakiEmpat = JSON.parse(getHewanKakiEmpat);
+
+// Langkah 3: Tampilkan di console
+console.log(hewanKakiEmpat);
